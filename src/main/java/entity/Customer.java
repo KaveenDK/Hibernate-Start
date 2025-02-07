3fdedce6ea3749402eb9583db84e7298f28efeda
KaveeN <vpdkkaveen@gmail.com>
2025-02-07 10:16:52 +0530

    updated hibernate.cfg.xml file

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -3,13 +3,20 @@ package entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Lombok annotations
 @Data // Lombok annotation to generate Getters, Setters, toString, equals, and hashCode
 @AllArgsConstructor
 @NoArgsConstructor
+
+//JPA annotations
 @Entity //map to table in database
 @Table(name = "customer") //table name in database
+
 public class Customer {
     @Id //Primary key
     private int id;
