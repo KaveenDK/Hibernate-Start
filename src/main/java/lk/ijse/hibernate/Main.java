d8217a7dc2c32350d634900c36e145f0bbac06c7
KaveeN <vpdkkaveen@gmail.com>
2025-02-07 11:55:17 +0530

    added saveCustomer, deleteCustomer, updateCustomer methods

--- src/main/java/lk/ijse/hibernate/Main.java
+++ src/main/java/lk/ijse/hibernate/Main.java
@@ -22,8 +22,19 @@ public class Main {
         } else {
             System.out.println("Failed to save the customer");
         }
-    }
 
+        Customer customerById = getCustomerById(5);
+        System.out.println(customerById);
+
+        deleteCustomer(5);
+
+        updateCustomer(2, new Customer(
+                2,
+                "dimantha",
+                "support@email.com",
+                0771234567
+        ));
+    }
 
     public static boolean saveCustomer(Customer customer) {
         Session session = FactoryConfiguration.getInstance().getSession();
@@ -31,7 +42,47 @@ public class Main {
             Transaction transaction = session.beginTransaction();
             session.save(customer);
             transaction.commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
             session.close();
+        }
+    }
+
+    public static Customer getCustomerById(int id) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Customer customer = session.get(Customer.class, id);
+        session.close();
+        return customer;
+    }
+
+    public static boolean deleteCustomer(int id) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        try {
+            Transaction transaction = session.beginTransaction();
+            Customer customer = session.get(Customer.class, id);
+            session.delete(customer);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            return false;
+        } finally {
+            session.close();
+        }
+    }
+
+    public static boolean updateCustomer(int id, Customer newCustomer) {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        try {
+            Customer customer = session.get(Customer.class, id);
+            Transaction transaction = session.beginTransaction();
+
+            customer.setName(newCustomer.getName());
+            customer.setEmail(newCustomer.getEmail());
+            customer.setPhone(newCustomer.getPhone());
+
+            transaction.commit();
             return true;
         } catch (Exception e) {
             return false;
